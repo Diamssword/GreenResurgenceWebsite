fd448a6fd12d4944474d14f585e7f72715b572a8
Diamssword <hugo.piechowiak@gmail.com>
2024-09-07 10:29:46 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,12 @@
 import "../app.css";
 </script>
 <svelte:head>
+    <meta property="og:title" content="Resurgence" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://resurgence.info" />
+    <meta property="og:image" content="https://resurgence.info/images/logo.png" />
+    <meta property="og:description" content="Serveur Minecraft post-apocalyptique" />
+    <meta name="theme-color" content="#5c9829">
     <title>{$SHARED.title?$SHARED.title+" |":""}Resurgence</title>
 </svelte:head>
 <div class="flex w-full h-screen flex-col">
