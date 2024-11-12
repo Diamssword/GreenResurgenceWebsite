1b82378c4d850e8ce9ae3746df127421f5806f33
Diamssword <hugo.piechowiak@gmail.com>
2024-11-12 22:28:52 +0100

    Update +server.ts

--- src/routes/modo/[id]/+server.ts
+++ src/routes/modo/[id]/+server.ts
@@ -1,5 +1,7 @@
-import type { RequestHandler } from './$types';
+import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async (req) => {
+    console.log(req.params.id)
+    console.log("requested");
     return new Response("1");
 };
