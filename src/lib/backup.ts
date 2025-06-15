e002bceea25a50e51109a16b124e4897a17bf8b8
Diamssword <hugo.piechowiak@gmail.com>
2025-06-15 22:33:18 +0200

    skills.json

--- src/lib/backup.ts
+++ src/lib/backup.ts
@@ -1,8 +1,8 @@
-import {Storage} from '@google-cloud/storage';
+//import {Storage} from '@google-cloud/storage';
 
 // For more information on ways to initialize Storage, please see
 // https://googleapis.dev/nodejs/storage/latest/Storage.html
 
 // Creates a client using Application Default Credentials
-const storage = new Storage();
+//const storage = new Storage();
 //await storage.bucket("test").upload("vite.config.ts",{destination:"test.txt"})
