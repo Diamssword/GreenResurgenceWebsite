1b51260520a57c0f6c029d04d4a10a7611bdbe64
Diamssword <hugo.piechowiak@gmail.com>
2025-08-14 16:49:08 +0200

    Update +page.ts

--- src/routes/editor/[id]/+page.ts
+++ src/routes/editor/[id]/+page.ts
@@ -4,6 +4,8 @@ import type { SkinPartsFormat,SkinLayersFormat } from './skin/skinTypes';
 const layers= layersOb as SkinLayersFormat[]
 export const load = (async (ev) => {
     var datas=await(await ev.fetch(import.meta.env.VITE_BASE_URL+"/skins/datas.json")).json();
+    console.log(import.meta.env.VITE_BASE_URL+"/skins/datas.json")
+    console.log(datas)
     var d= datas as {[key:string]:SkinPartsFormat}
     return {datas:d,sheet:ev.data.sheet,layers};
 }) satisfies PageLoad;
