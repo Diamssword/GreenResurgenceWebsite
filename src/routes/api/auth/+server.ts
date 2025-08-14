ff2007d0adf692518085707f8ebafc4a827fccbd
Diamssword <hugo.piechowiak@gmail.com>
2025-08-14 17:23:18 +0200

    Update +server.ts

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,12 +1,13 @@
 import { error, json, type RequestEvent } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import jwt from 'jsonwebtoken'
+import { API_KEY, API_TOKEN } from '$env/static/private';
 export const POST: RequestHandler = async (ev) => {
     
     var js=await ev.request.json();
-    if(js.key==import.meta.env.API_TOKEN)
+    if(js.key==API_TOKEN)
     {
-            var signed=jwt.sign({valid:1},import.meta.env.API_KEY,{expiresIn:"2h"})
+            var signed=jwt.sign({valid:1},API_KEY,{expiresIn:"2h"})
             return json({token:signed},{status:200})
     }
     return error(403);
