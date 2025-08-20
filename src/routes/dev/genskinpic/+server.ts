6957d3bda22c9efac002c94c524f4e4c8f088a24
Diamssword <hugo.piechowiak@gmail.com>
2025-08-20 11:32:52 +0200

    typo

--- src/routes/dev/genskinpic/+server.ts
+++ src/routes/dev/genskinpic/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from './$types';
 import * as fs from 'fs'
 import * as path from 'path'
 if(!fs.existsSync("./datas/skins_display/"))
-    fs.mkdirSync("./datas/skins_display/",{recursive:trye})
+    fs.mkdirSync("./datas/skins_display/",{recursive:true})
 export const POST: RequestHandler = async (ev) => {
     var dt=await ev.request.json();
     if(dev && dt.id && dt.image){
